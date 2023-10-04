31a8811dbca2d84e362deec9216037bf23e991d9
Misha Gordeico <misha.gordeico@gmail.com>
2023-10-04 21:37:03 +0300

    add takaya-to zalupa

--- weq.py
+++ weq.py
@@ -1,6 +1,10 @@
 def f(i):
     l = i.count(',')
     if l+1 > 20:
+        if 1>2:
+            if 1 > 2:
+                if 1 > 2:
+                    if 1 > 2:
         return True
     else:
         return False
