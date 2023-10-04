748c8712d44faeac6dec5e1e470dc5eebfb422a7
Misha Gordeico <misha.gordeico@gmail.com>
2023-10-04 21:43:19 +0300

    123

--- weq.py
+++ weq.py
@@ -1,6 +1,10 @@
 def f(i):
     l = i.count(',')
     if l+1 > 20:
+        for i in 5:
+            for i in 5:
+                for i in 5:
+                    for i in 5:
         return True
     else:
         return False
